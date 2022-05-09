9c0da1f23809afca246f6e28648c80e624b29273
Fer <30288888+ferboiar@users.noreply.github.com>
2022-05-09 09:29:38 +0200

    Update rebootschedule.lua

--- luasrc/model/cbi/rebootschedule.lua
+++ luasrc/model/cbi/rebootschedule.lua
@@ -1,10 +1,10 @@
 m=Map("rebootschedule",translate("定时任务设置"),
-translate("<font color=\"red\"><b>让计划任务更加易用的插件，可以使用-表示连续的时间范围，使用,表示不连续的多个时间点，使用*/表示循环执行。可以使用“添加”来添加多条计划任务命令。可使用“--自定义--”来自行添加其它参数。</b></font></br>") ..
-translate("*所有时间参数都是指该自然单位中的时间点，而非累积计数，比如星期只能是0～6，小时只能是0～23，分钟只能是0～59，不能使用48小时、80分钟这种累积计数表示法。</br>") ..
+translate("让计划任务更加易用的插件，可以使用-表示连续的时间范围，使用,表示不连续的多个时间点，使用*/表示循环执行。可以使用“添加”来添加多条计划任务命令。可使用“--自定义--”来自行添加其它参数。</br></br>") ..
+translate("* 所有时间参数都是指该自然单位中的时间点，而非累积计数，比如星期只能是0～6，小时只能是0～23，分钟只能是0～59，不能使用48小时、80分钟这种累积计数表示法。</br>") ..
 translate("* 所有数值可使用 - 连接表示连续范围，比如星期：1-5 表示星期一至星期五；使用,表示不连续的点，比如星期：1,3,5 表示仅仅星期一、三、五。") ..
 translate("&nbsp;&nbsp;&nbsp;<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\"" ..
 translate("查看/验证") ..
-" \" onclick=\"window.open('https://tool.lu/crontab/')\"/>")
+" \" onclick=\"window.open('http://www.cronmaker.com/?1')\"/>")
 )
 
 s=m:section(TypedSection,"crontab","")
@@ -19,7 +19,7 @@ p.rmempty = false
 p.default=0
 
 
-week=s:option(Value,"week",translate("星期 <font color=\" red\">(数值范围0～6)</font>"),
+week=s:option(Value,"week",translate("星期 (数值范围0～6)"),
 translate("<font color=\"gray\">和日期是逻辑“与”关系</br>n1-n5连续，n1,n3,n5不连续</font>"))
 week.rmempty = true
 week:value('*',translate("每天"))
@@ -33,17 +33,17 @@ week:value(6,translate("Saturday"))
 week.default='*'
 
 
-hour=s:option(Value,"hour",translate("时 <font color=\" red\">(数值范围0～23)</font>"),
+hour=s:option(Value,"hour",translate("时 (数值范围0～23)"),
 translate("<font color=\"gray\">*表示每小时，*/n表示每n小时</br>n1-n5连续，n1,n3,n5不连续</font>"))
 hour.rmempty = false
 hour.default = '5'
 
-minute=s:option(Value,"minute",translate("分 <font color=\" red\">(数值范围0～59)</font>"),
+minute=s:option(Value,"minute",translate("分 (数值范围0～59)"),
 translate("<font color=\"gray\">*表示每分钟，*/n表示每n分钟</br>n1-n5连续，n1,n3,n5不连续</font>"))
 minute.rmempty = false
 minute.default = '0'
 
-command=s:option(Value,"command",translate("执行命令 <font color=\" red\">(多条用 && 连接)</font>"),
+command=s:option(Value,"command",translate("执行命令 (多条用 && 连接)"),
 translate("<font color=\"gray\">按“--自定义--”可进行修改</br>(亦可添加后到计划任务中修改)</font>"))
 command:value('sync && echo 3 > /proc/sys/vm/drop_caches', translate("A.释放内存"))
 command:value('sysfree.sh',translate("B.清理垃圾"))
